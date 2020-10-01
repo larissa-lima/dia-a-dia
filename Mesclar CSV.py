d1be6737641c8de61c477343ad521cc46405cff9
larissa-lima <71884306+larissa-lima@users.noreply.github.com>
2020-10-01 11:42:22 -0300

    Add files via upload

---
+++ Mesclar CSV.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from pathlib import Path
+
+#caminho dos CSV
+source_files = sorted(Path('C:\Temp\MergeFIles').glob('*.csv'))
+
+dataframes = []
+for file in source_files:
+    df = pd.read_csv(file) # additional arguments up to your need
+    df['source'] = file.name
+    dataframes.append(df)
+
+#caminho para salvar + nome do arquivo
+all = pd.concat(dataframes).to_csv("C:/Users/A353375/Desktop/Merge Files"+"/"+"merged.csv")
